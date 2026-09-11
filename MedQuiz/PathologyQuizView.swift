26888346efe48e52bfdd1f7a7f5e91aef9d6a802
Dinara <dina.mustafina2004@yandex.ru>
2026-09-11 19:49:25 +0300

    Add pathology quiz with answer explanations

---
+++ MedQuiz/PathologyQuizView.swift
@@ -0,0 +1,18 @@
+//
+//  PathologyQuizView.swift
+//  MedQuiz
+//
+//  Created by Динара on 11.09.2026.
+//
+
+import SwiftUI
+
+struct PathologyQuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PathologyQuizView()
+}
