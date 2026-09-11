3da185a175aae6cbb9de9a6eec6b783e818329ac
Dinara <dina.mustafina2004@yandex.ru>
2026-09-11 19:29:46 +0300

    Initial Commit

---
+++ MedQuiz/MedQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedQuizApp.swift
+//  MedQuiz
+//
+//  Created by Динара on 11.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct MedQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
